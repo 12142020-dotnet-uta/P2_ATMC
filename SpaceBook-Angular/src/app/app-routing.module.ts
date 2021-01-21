bd06f12915b297cce9ccda2c83d29b2c2d12a8f0
Alan Munoz <alanvz825@gmail.com>
2021-01-21 10:56:48 -0600

    Adding the Router Module and 2 components, a dashboard and a gallery

---
+++ SpaceBook-Angular/src/app/app-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+import { DashboardComponent } from "./components/dashboard/dashboard.component";
+import { GalleryComponent } from "./components/gallery/gallery.component";
+
+import { RouterModule,Routes } from "@angular/router";
+
+const routes : Routes = [
+  //Default route for navigation
+  {path: '', redirectTo:'/dashboard', pathMatch:'full' },
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'gallery',component:GalleryComponent},
+];
+
+
+@NgModule({
+  exports: [ RouterModule ],
+  imports: [
+    RouterModule.forRoot(routes)
+  ]
+  // , 
+})
+export class AppRoutingModule { }
--- SpaceBook-Angular/src/app/app.module.ts
+++ SpaceBook-Angular/src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { GalleryComponent } from './components/gallery/gallery.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GalleryComponent,
+    DashboardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
