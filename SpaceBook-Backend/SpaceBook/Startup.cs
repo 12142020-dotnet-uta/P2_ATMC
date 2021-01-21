12f8eb52bd56508591c19d001821515c9cdef0cf
trvrgrhm <trvrgrhm@gmail.com>
2021-01-21 09:50:55 -0700

    adds repository and changes startup

--- SpaceBook-Backend/SpaceBook/Startup.cs
+++ SpaceBook-Backend/SpaceBook/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using SpaceBook.Repository;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -24,6 +25,9 @@ namespace SpaceBook
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
+            services.AddScoped<ApplicationDbContext>();
+
+            services.AddScoped<PictureRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
