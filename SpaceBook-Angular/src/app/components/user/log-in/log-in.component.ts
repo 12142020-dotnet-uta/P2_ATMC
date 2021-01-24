4227e094235a2ad43a3bc20292715bb739736236
Alan Munoz <alanvz825@gmail.com>
2021-01-23 23:18:43 -0600

    Reviewing the LogIn Component, using divs for the form group, i will change it into form

--- SpaceBook-Angular/src/app/components/user/log-in/log-in.component.ts
+++ SpaceBook-Angular/src/app/components/user/log-in/log-in.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { UserLogIn } from "../../../interfaces/user-log-in";
+import { UserRegister } from "../../../interfaces/user-register";
+
 
 @Component({
   selector: 'app-log-in',
@@ -10,17 +13,29 @@ export class LogInComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    
   }
 
-  userLogIn = true;
-  userRegister = false;
+  boolLogIn = true;
+  boolRegister = false;
+  userLogIn: UserLogIn;
+  userRegister: UserRegister;
+
+  strFirstName:string;
+  strLastName:string;
+  strLogInUserName:string;
+  strLogInPassword:string;
+  strEmail: string;
+  
+  strUserName:string;
+  strPassword:string;
 
   /* The user object that will be used over here */
 
   onChangingSection()
   {
-    this.userLogIn = !this.userLogIn;
-    this.userRegister = !this.userRegister;
+    this.boolLogIn = !this.boolLogIn;
+    this.boolRegister = !this.boolRegister;
   
   }
 
@@ -28,11 +43,30 @@ export class LogInComponent implements OnInit {
   RegisteringUser(){
 
     //
+    this.userRegister = {
+      UserID : '',
+      UserName : this.strUserName,
+      Password : this.strPassword,
+      FirstName : this.strPassword,
+      LastName : this.strLastName,
+      Email: this.strEmail
+    }
+    console.log(this.userLogIn);
+    
   }
 
   LogIngUser()
   {
 
+    this.userLogIn= {
+      UserID : '',
+      UserName : this.strUserName,
+      Password : this.strPassword,
+      FirstName : this.strPassword,
+      LastName : this.strLastName
+    }
+
+    console.log(this.userRegister);
   }
 
 }
