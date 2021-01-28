55ba8d43cf2f85617f60e800e90a80641dcb3973
Alan Munoz <alanvz825@gmail.com>
2021-01-28 15:38:16 -0600

    working on the uploadPicture

---
+++ SpaceBook-Backend/SpaceBook.Models/ViewModels/UserPictureViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpaceBook.Models.ViewModels
+{
+    public class UserPictureViewModel
+    {
+        public string username { get; set; }
+        public string title { get; set; }
+        public string description { get; set; }
+        public byte[] fileAsBase64 { get; set; }
+
+
+    }
+}
